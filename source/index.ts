34b7d8e999c2c9f922a75b0d2d38755173faf98d
TechQuery <shiy007@qq.com>
2021-12-26 06:28:56 +0800

    [fix] duplicated Default exports (parcel-bundler/parcel#7483) [fix] Scope hoist (parcel-bundler/parcel#7176)

--- source/index.ts
+++ source/index.ts
@@ -1,9 +1,9 @@
-export * from './TimeDistance';
-export * from './PaginationBar';
-export * from './Icon';
-export * from './Avatar';
-export * from './Nameplate';
-export * from './FilterInput';
-export * from './FilePicker';
-export * from './Editor';
-export * from './EditorHTML';
+export { TimeDistanceProps, TimeDistance } from './TimeDistance';
+export { PaginationBarProps, PaginationBar } from './PaginationBar';
+export { IconProps, Icon } from './Icon';
+export { AvatarProps, Avatar } from './Avatar';
+export { NameplateProps, Nameplate } from './Nameplate';
+export { Option, FilterInputProps, FilterInput } from './FilterInput';
+export { FilePickerProps, FilePicker } from './FilePicker';
+export { EditorProps, Editor } from './Editor';
+export { parser, EditorHTMLProps, EditorHTML } from './EditorHTML';
