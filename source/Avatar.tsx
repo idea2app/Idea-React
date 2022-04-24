73bd028b8a6ee42e5b5f2bf3a48d41a886a54bb5
赵苓利 <zhaolinglizll@qq.com>
2022-04-24 20:09:22 +0800

    [optimize Avatar]  加上 classnam [optimize IdeaDialog]  加上classname; 加上漏掉的onConfirm [optimize IdeaInfo] 去掉默认classname

--- source/Avatar.tsx
+++ source/Avatar.tsx
@@ -1,18 +1,21 @@
+import classNames from 'classnames';
 import React, { CSSProperties, FC, PropsWithoutRef } from 'react';
 
 export type AvatarProps = PropsWithoutRef<{
     style?: CSSProperties;
+    className?: string;
     src?: string;
     size?: number;
 }>;
 
 export const Avatar: FC<AvatarProps> = ({
     style,
+    className,
     src = '/typescript.png',
     size = 3
 }) => (
     <img
-        className="rounded-circle"
+        className={classNames(className, 'rounded-circle')}
         style={{
             width: `${size}rem`,
             height: `${size}rem`,
