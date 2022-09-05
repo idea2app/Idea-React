6cc9b2819e0c5e9f05a5b4baf7628bc914032461
TechQuery <shiy2008@gmail.com>
2022-09-06 04:08:48 +0800

    [fix] remove "import" statements in CJS bundle

--- bundle-fix.ts
+++ bundle-fix.ts
@@ -2,19 +2,28 @@
 
 import { promises } from 'fs';
 
-const list = [
-    { file: 'dist/index.js', pattern: 'import "./index.css";' },
-    { file: 'dist/index.umd.js', pattern: 'require("./index.umd.css");' }
-];
+const list = process.argv.slice(2);
+
+const CJS_pattern = /require\(('|").+?('|")\)/;
 
 (async () => {
     console.log();
     console.time('Bundle fix');
 
-    for (const { file, pattern } of list) {
-        const code = await promises.readFile(file, { encoding: 'utf-8' });
+    for (const file of list) {
+        let code = await promises.readFile(file, { encoding: 'utf-8' });
 
-        await promises.writeFile(file, code.replace(pattern, '').trim());
+        const isCJS = CJS_pattern.test(code);
+
+        const CSS_pattern = isCJS
+            ? /require\(('|").+?\.css('|")\);/g
+            : /import\s*('|").+?\.css('|");/g;
+
+        code = code.replace(CSS_pattern, '').trim();
+
+        if (isCJS) code = code.replace(/import(".+?")/g, 'require($1)');
+
+        await promises.writeFile(file, code);
 
         console.log(`[fixed] ${file}`);
     }
