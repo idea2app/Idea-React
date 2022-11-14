5cc8612d1470797602065ce132f283f101fa6b94
soecka <32708404+Soecka@users.noreply.github.com>
2022-11-15 01:45:24 +0800

    [polish] IdeaDialog props

--- source/IdeaDialog.tsx
+++ source/IdeaDialog.tsx
@@ -1,30 +1,28 @@
 import { FC } from 'react';
-import { Button, Modal, ModalDialog, ModalDialogProps } from 'react-bootstrap';
+import { Button, Modal, ModalProps } from 'react-bootstrap';
 
 import { SpinnerButtonProps, SpinnerButton } from './SpinnerButton';
 
 export interface IdeaDialogProps
-    extends Partial<Pick<SpinnerButtonProps, 'animation' | 'loading'>> {
+    extends Partial<
+        Pick<SpinnerButtonProps, 'animation' | 'loading'> &
+            Pick<
+                ModalProps,
+                | 'className'
+                | 'show'
+                | 'size'
+                | 'fullscreen'
+                | 'centered'
+                | 'scrollable'
+                | 'contentClassName'
+            >
+    > {
     title: string;
-    show: boolean;
-    className?: string;
     formId?: string;
     confirmText?: string;
     onConfirm?: () => any;
     cancelText?: string;
     onCancel?: () => any;
-    size?: 'sm' | 'lg' | 'xl';
-    fullscreen?:
-        | true
-        | string
-        | 'sm-down'
-        | 'md-down'
-        | 'lg-down'
-        | 'xl-down'
-        | 'xxl-down';
-    centered?: boolean;
-    scrollable?: boolean;
-    contentClassName?: string;
 }
 
 export const IdeaDialog: FC<IdeaDialogProps> = ({
@@ -37,48 +35,42 @@ export const IdeaDialog: FC<IdeaDialogProps> = ({
     onCancel,
     animation = 'border',
     loading,
-    show,
     ...rest
 }) => (
-    <>
-        <div className="fade modal-backdrop show" />
-        <div className="show modal fade" style={{ display: 'block' }}>
-            <ModalDialog {...rest}>
-                <Modal.Header closeButton onHide={onCancel}>
-                    <Modal.Title>{title}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>{children}</Modal.Body>
+    <Modal {...rest} onHide={onCancel}>
+        <Modal.Header closeButton>
+            <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{children}</Modal.Body>
 
-                {(confirmText || cancelText) && (
-                    <Modal.Footer>
-                        {cancelText && (
-                            <Button
-                                variant="secondary"
-                                type="reset"
-                                form={formId}
-                                disabled={loading}
-                                onClick={onCancel}
-                            >
-                                {cancelText}
-                            </Button>
-                        )}
-                        {confirmText && (
-                            <SpinnerButton
-                                variant="primary"
-                                type="submit"
-                                form={formId}
-                                animation={animation}
-                                loading={loading}
-                                onClick={onConfirm}
-                            >
-                                {confirmText}
-                            </SpinnerButton>
-                        )}
-                    </Modal.Footer>
+        {(confirmText || cancelText) && (
+            <Modal.Footer>
+                {cancelText && (
+                    <Button
+                        variant="secondary"
+                        type="reset"
+                        form={formId}
+                        disabled={loading}
+                        onClick={onCancel}
+                    >
+                        {cancelText}
+                    </Button>
                 )}
-            </ModalDialog>
-        </div>
-    </>
+                {confirmText && (
+                    <SpinnerButton
+                        variant="primary"
+                        type="submit"
+                        form={formId}
+                        animation={animation}
+                        loading={loading}
+                        onClick={onConfirm}
+                    >
+                        {confirmText}
+                    </SpinnerButton>
+                )}
+            </Modal.Footer>
+        )}
+    </Modal>
 );
 
 IdeaDialog.displayName = 'IdeaDialog';
