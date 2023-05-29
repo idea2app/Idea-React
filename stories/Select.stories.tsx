c4a1eeb359e4ab42265072dac9c9fd60f16629e7
stevending1st <stevending1st@163.com>
2023-05-29 12:18:56 +0800

    chore(Select): add storybook cases

---
+++ stories/Select.stories.tsx
@@ -0,0 +1,53 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Select, Option } from '../source/Select';
+import { useState } from 'react';
+
+const meta: Meta<typeof Select> = {
+    title: 'Idea-React/Select',
+    component: Select,
+    tags: ['autodocs'],
+    argTypes: {
+        className: { control: 'text' },
+        style: { control: 'object' },
+        value: { control: 'text' },
+        children: { control: 'text' },
+        variant: { control: 'text' },
+        menuVariant: { control: 'text' }
+    }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Select>;
+
+export const Primary: Story = {
+    render: ({ value, onChange, ...otherArgs }) => {
+        const [selectValue, setValue] = useState<string>(value);
+        return (
+            <Select
+                key="999999"
+                value={selectValue}
+                onChange={item => {
+                    onChange?.(item);
+                    setValue(item);
+                }}
+                {...otherArgs}
+            >
+                <Option value="0">idea2app/Idea-React</Option>
+                <Option value="1">idea2app/event-submitter-polyfill</Option>
+            </Select>
+        );
+    },
+    args: {
+        value: '0'
+    }
+};
+
+export const Bigger: Story = {
+    render: Primary.render,
+    args: {
+        ...Primary.args,
+        className: 'fw-bolder'
+    }
+};
