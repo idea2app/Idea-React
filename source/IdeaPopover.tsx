e06e0f54970e518841b00c571124f602e2458ca2
TechQuery <shiy2008@gmail.com>
2023-06-08 19:37:04 +0800

    [refactor] rewrite IdeaPopover to OverlayBox with Tooltip & Popover supporting [optimize] update Upstream packages

--- source/IdeaPopover.tsx
+++ source/IdeaPopover.tsx
@@ -1,57 +1,37 @@
-import { createRef, PropsWithChildren, PureComponent } from 'react';
-import { Overlay, Popover } from 'react-bootstrap';
+import { FC, ReactNode } from 'react';
+import {
+    OverlayTrigger,
+    OverlayTriggerProps,
+    Popover,
+    Tooltip
+} from 'react-bootstrap';
 
-interface State {
-    showPopover: boolean;
-    popoverTarget: EventTarget;
+export interface OverlayBoxProps extends OverlayTriggerProps {
+    title: ReactNode;
+    detail?: ReactNode;
 }
 
-export type IdeaPopoverProps = PropsWithChildren<{
-    onShow?: () => any;
-    title: string;
-}>;
+export const OverlayBox: FC<OverlayBoxProps> = ({
+    title,
+    detail,
+    children,
+    ...props
+}) => (
+    <OverlayTrigger
+        {...props}
+        overlay={
+            detail ? (
+                <Popover>
+                    <Popover.Header>{title}</Popover.Header>
+                    <Popover.Body>{detail}</Popover.Body>
+                </Popover>
+            ) : (
+                <Tooltip>{title}</Tooltip>
+            )
+        }
+    >
+        {children}
+    </OverlayTrigger>
+);
 
-export class IdeaPopover extends PureComponent<IdeaPopoverProps, State> {
-    static displayName = 'IdeaPopover';
-
-    popoverRef = createRef<HTMLDivElement>();
-
-    state = {
-        showPopover: false,
-        popoverTarget: {} as HTMLDivElement
-    };
-
-    handleClick = async (event: React.MouseEvent) => {
-        const { showPopover } = this.state;
-
-        this.setState({
-            popoverTarget: event.target
-        });
-        if (!showPopover) this.props.onShow?.();
-
-        this.setState({ showPopover: !showPopover });
-    };
-
-    render() {
-        const { showPopover, popoverTarget } = this.state;
-        const { title, children } = this.props;
-
-        return (
-            <div ref={this.popoverRef}>
-                <div onClick={this.handleClick}>{children?.[0]}</div>
-
-                <Overlay
-                    show={showPopover}
-                    target={popoverTarget}
-                    placement="right"
-                    container={this.popoverRef}
-                >
-                    <Popover id="popover-contained">
-                        <Popover.Header as="h3">{title}</Popover.Header>
-                        <Popover.Body>{children?.[1]}</Popover.Body>
-                    </Popover>
-                </Overlay>
-            </div>
-        );
-    }
-}
+OverlayBox.displayName = 'OverlayBox';
