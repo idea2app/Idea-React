f212b608cfc280d8011a84aa3995eb7428a804d6
TechQuery <shiy2008@gmail.com>
2024-10-19 00:09:48 +0800

    [fix] some CSS interface details of User Rank component

--- source/UserRank/index.tsx
+++ source/UserRank/index.tsx
@@ -1,5 +1,5 @@
 import { PureComponent } from 'react';
-import { Badge, Col, Image, Row, Table } from 'react-bootstrap';
+import { Badge, Col, Image, Row, RowProps, Table } from 'react-bootstrap';
 
 import { UserAddress, UserAddressProps } from './Address';
 import * as style from './index.module.less';
@@ -11,7 +11,7 @@ export interface UserRank extends UserAddressProps {
     score: number;
 }
 
-export interface UserRankViewProps {
+export interface UserRankViewProps extends RowProps {
     title: string;
     rank: UserRank[];
     linkOf?: (user: UserRank) => string;
@@ -19,14 +19,14 @@ export interface UserRankViewProps {
 
 export class UserRankView extends PureComponent<UserRankViewProps> {
     renderMedal = (user: UserRank) => (
-        <Col as="li" key={user.id}>
+        <Col key={user.id} as="li">
             <div
-                className={`shadow-lg overflow-hidden rounded-circle m-auto ${style.imgBox}`}
+                className={`shadow-lg overflow-hidden rounded-circle m-auto ${style.image}`}
             >
-                <Image fluid src={user.avatar} alt={user.name} loading="lazy" />
+                <Image src={user.avatar} alt={user.name} loading="lazy" fluid />
             </div>
             <div
-                className={`position-relative overflow-hidden ${style.showBox}`}
+                className={`position-relative overflow-hidden ${style.topUser}`}
             >
                 <div className="position-relative">
                     <i className="d-block overflow-hidden m-auto mb-1 rounded-circle" />
@@ -47,20 +47,20 @@ export class UserRankView extends PureComponent<UserRankViewProps> {
         <tr key={user.id} className="position-relative">
             <td className="align-middle">
                 <Badge className="fw-bold fst-italic">
-                    {(index + 4 + '').padStart(length, '0')}
+                    {(index + 4 + '').padStart((length + '').length, '0')}
                 </Badge>
             </td>
             <td
-                className={`position-relative mw-50 text-truncate ${style.usernameBox}`}
+                className={`position-relative mw-50 text-truncate ${style.name}`}
             >
                 <div
-                    className={`d-inline-block overflow-hidden align-middle rounded-circle ${style.imgBox}`}
+                    className={`d-inline-block overflow-hidden align-middle rounded-circle ${style.image}`}
                 >
                     <Image
-                        fluid
                         src={user.avatar}
                         alt={user.name}
                         loading="lazy"
+                        fluid
                     />
                 </div>
                 <a
@@ -81,13 +81,13 @@ export class UserRankView extends PureComponent<UserRankViewProps> {
     );
 
     render() {
-        const { title, rank = [] } = this.props;
+        const { className = '', title, rank = [], ...props } = this.props;
 
         return (
-            <Row className={style.topUserRow}>
+            <Row className={`${style.rankBox} ${className}`} {...props}>
                 <Col sm={12} xs={12} lg={7}>
-                    <div className={`shadow rounded-3 ${style.showTitle}`}>
-                        <div className={style.showMedal}>
+                    <div className={`shadow rounded-3 ${style.title}`}>
+                        <div className={style.medal}>
                             <i className="d-inline-block ms-1 overflow-hidden rounded-circle" />
                             <i className="d-inline-block ms-1 overflow-hidden rounded-circle" />
                             <i className="d-inline-block ms-1 overflow-hidden rounded-circle" />
@@ -101,15 +101,15 @@ export class UserRankView extends PureComponent<UserRankViewProps> {
                     </div>
                     <Row
                         as="ul"
-                        className={`mt-2 g-0 align-items-end text-center ps-0 pt-2 list-unstyled ${style.topUserUl}`}
+                        className={`mt-2 g-0 align-items-end text-center ps-0 pt-2 list-unstyled ${style.topUsers}`}
                     >
                         {rank.slice(0, 3).map(this.renderMedal)}
                     </Row>
                 </Col>
                 <Col xs={12} sm={12} lg={5}>
                     <Table
+                        className={`my-3 pt-2 ${style.restUsers}`}
                         responsive
-                        className={`my-3 pt-2 ${style.topUserList}`}
                     >
                         <tbody>{rank.slice(3, 10).map(this.renderRow)}</tbody>
                     </Table>
